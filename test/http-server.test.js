e5b869198351be19187d82e4b40c9505f456bae5
meghanroserebecca <meghanroserebecca@gmail.com>
2017-02-03 12:53:21 -0800

    wrote test for GET /greeting to test to see if get/greeting results in 'hello stranger' response text;

--- test/http-server.test.js
+++ test/http-server.test.js
@@ -1,10 +1,23 @@
-const server = require('..lib/http-server');
+const server = require('../lib/http-server');
 
 const chai = require('chai');
 const assert = chai.assert;
 const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
+
+describe('testing http servers with chai-http', () => {
+    const request = chai.request(server);
+
+    it('GET /greeting says', done => {
+        request
+            .get('/greeting')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'hello stranger!');
+                done();
+            });
+    });
+});
 
 
 
